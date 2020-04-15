3c07ce3417d1370337bf37b19105dbf84b26dc6a
arun kumar <alekhya0801@gmail.com>
2020-04-15 14:20:01 +0530

    Added feature to move position

--- snakeAndLadderSimulator.sh
+++ snakeAndLadderSimulator.sh
@@ -1,4 +1,21 @@
 #!/bin/bash -x
-NumOfPlayers=1
+#constants
+MOVE_FORWARD=1
+MOVE_BACKWARD=2
+NO_PLAY=0
+NUMBER_OF_PLAYERS=1
+#variables
 position=0
 dieNumber=$((RANDOM%6+1))
+random=$((RANDOM%3))
+case $random in
+	$MOVE_FORWARD)
+		position=$(($position+$dieNumber))
+	;;
+        $MOVE_BACKWARD)
+                position=$(($position-$dieNumber))
+        ;;
+        $NO_PLAY)
+                position=$(($position+0))
+        ;;
+esac
