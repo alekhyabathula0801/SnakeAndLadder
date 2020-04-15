d18075153fd41d4527f5b1035d50720628b1a2eb
arun kumar <alekhya0801@gmail.com>
2020-04-15 15:00:04 +0530

    Added feature to reach goal

--- snakeAndLadderSimulator.sh
+++ snakeAndLadderSimulator.sh
@@ -4,18 +4,24 @@ MOVE_FORWARD=1
 MOVE_BACKWARD=2
 NO_PLAY=0
 NUMBER_OF_PLAYERS=1
-#variables
-position=0
-dieNumber=$((RANDOM%6+1))
-random=$((RANDOM%3))
-case $random in
-	$MOVE_FORWARD)
-		position=$(($position+$dieNumber))
-	;;
-        $MOVE_BACKWARD)
-                position=$(($position-$dieNumber))
-        ;;
-        $NO_PLAY)
-                position=$(($position+0))
-        ;;
-esac
+GOAL=100
+START_POSITION=0
+
+position=$START_POSITION
+while [ $position -lt $GOAL ]
+do
+	dieNumber=$((RANDOM%6+1))
+	random=$((RANDOM%3))
+	case $random in
+		$MOVE_FORWARD)
+			position=$(($position+$dieNumber))
+		;;
+        	$MOVE_BACKWARD)
+                	position=$(($position-$dieNumber))
+        		if [ $position -lt $START_POSITION ]
+			then
+				position=$START_POSITION
+			fi
+		;;
+	esac
+done
