3b5100f01cf6c66cba90efcea647cfe100e50599
arun kumar <alekhya0801@gmail.com>
2020-04-15 19:11:38 +0530

    Added feature to declare winner between two players

--- snakeAndLadderSimulator.sh
+++ snakeAndLadderSimulator.sh
@@ -3,38 +3,53 @@
 MOVE_FORWARD=1
 MOVE_BACKWARD=2
 NO_PLAY=0
-NUMBER_OF_PLAYERS=1
+NUMBER_OF_PLAYERS=2
 GOAL=100
 START_POSITION=0
 #variable
-numOfRolls=0
+playNextTurn=yes
+declare -A position
 
-position[$numOfRolls]=$START_POSITION
-while [ ${position[$numOfRolls]} -lt $GOAL ]
+for (( player=1; player<=$NUMBER_OF_PLAYERS; player++ ))
 do
-	prevRoll=$numOfRolls
-	numOfRolls=$(($numOfRolls+1))
-	dieNumber=$((RANDOM%6+1))
-	random=$((RANDOM%3))
-	case $random in
-		$MOVE_FORWARD)
-			position[$numOfRolls]=$((${position[$prevRoll]}+$dieNumber))
-                        if [ ${position[$numOfRolls]} -gt $GOAL ]
-                        then
-                                position[$numOfRolls]=${position[$prevRoll]}
-                        fi
-		;;
-        	$MOVE_BACKWARD)
-                	position[$numOfRolls]=$((${position[$prevRoll]}-$dieNumber))
-        		if [ ${position[$numOfRolls]} -lt $START_POSITION ]
-			then
-				position[$numOfRolls]=$START_POSITION
-			fi
-		;;
-		*)
-			position[$numOfRolls]=${position[$prevRoll]}
-		;;
-	esac
+	position[$player]=$START_POSITION
 done
-echo Total number of die rolls = $numOfRolls
-echo For die roll numbers ${!position[@]} positions are ${position[@]} respectively
+
+function findPosition(){
+	for (( player=1; player<=$NUMBER_OF_PLAYERS; player++ ))
+        do
+		dieNumber=$((RANDOM%6+1))
+		random=$((RANDOM%3))
+		case $random in
+			$MOVE_FORWARD)
+				position[$player]=$((${position[$player]}+$dieNumber))
+                        	if [ ${position[$player]} -gt $GOAL ]
+                        	then
+                                	position[$player]=$((${position[$player]}-$dieNumber))
+                        	fi
+			;;
+        		$MOVE_BACKWARD)
+                		position[$player]=$((${position[$player]}-$dieNumber))
+        			if [ ${position[$player]} -lt $START_POSITION ]
+				then
+					position[$player]=$START_POSITION
+				fi
+			;;
+		esac
+	done
+}
+
+while [ $playNextTurn = yes ]
+do
+	#calling function to find position of players
+	findPosition
+	for (( player=1; player<=$NUMBER_OF_PLAYERS; player++ ))
+	do
+		if [ ${position[$player]} -eq $GOAL ]
+		then
+			winner=$player
+			playNextTurn=no
+		fi
+	done
+done
+echo winner is $winner
